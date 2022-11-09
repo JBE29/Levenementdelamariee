609737897db34cc8ff91119f5d5ff69ff3dc1bdf
JBE29 <jeanbaptiste.emily@gmail.com>
2022-11-09 07:22:09 +0100

    with event index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
     has_many :attendances, dependent: :destroy
     has_many :users, through: :attendances
-    belongs_to :event_admin, class_name: "User"
+    #belongs_to :event_admin, class_name: "User"
 
     validates :start_date, presence: true
     validates :duration, presence: true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-config.action_mailer.default_url_options = { :host => 'levenementdelamariee.herokuapp.com' }
+#config.action_mailer.default_url_options = { :host => 'levenementdelamariee.herokuapp.com' }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
